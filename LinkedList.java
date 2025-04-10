a773a6cb6375de6d1925a34b1f069686fb675ce5
MaxBennett2024 <maximus.bennett@mcusd709.org>
2025-04-10 08:33:47 -0500

    mm

--- LinkedList.java
+++ LinkedList.java
@@ -5,7 +5,7 @@ public class LinkedList {
 
     //default constructor
     public LinkedList(){
-        this.thing = new Node(0);
+        this.thing = null;
     }
 
     //returns the head of the linked list
@@ -15,31 +15,61 @@ public class LinkedList {
 
     //adds a node to the end of the linked list
     public void append(Node node){
-        Node temp = this.thing;
-        while(thing.getNext()!=null){
-            temp = thing.getNext();
+        Node temp = thing;
+        if(temp!=null){
+            while(temp.getNext()!=null){
+                temp = temp.getNext();
+            }
+            temp.setNext(node);
+        }else{
+            thing = node;
         }
-        temp.setNext(node);
     }
     
     //inserts a node at the given index or appends to the end if the index is out of bounds
     public void insert(Node node, int index){
         int i = index;
-        while(i<=index){
-            thing.getNext();
+        Node temp = thing;
+        if(temp!=null){
+            while(i<=index&&temp.getNext()!=null){
+                temp = temp.getNext();
+           }
+           Node balls = temp.getNext();
+           temp.setNext(node);
+           node.setNext(balls);
         }
-        thing.setNext(node);
-        node.setNext(thing);
     }
 
     //removes and then returns the node at a given index, null if index out of bounds
     public Node remove(int index){
+        int i = index;
+        Node temp = thing;
+        if(temp!=null){
+            while(i<=index&&temp.getNext().getNext()!=null){
+                temp = temp.getNext();
+           }
+           Node balls = temp.getNext();
+           temp.setNext(temp.getNext().getNext());
+           return balls;
+        }
         return null;
     }
     
     //returns information about the linked list
     public String toString(){
         String result = "";
+        int count = 0;
+        Node temp = thing;
+        if(thing!=null){
+            while(temp.getNext()!=null){
+                result += "Index: " + count + " Value: " + temp.getData() + "\n";
+                count++;
+                temp = temp.getNext();
+            }
+        }else{
+            result = "There are no items in this list.";
+        }
+        
         return result;
     }
 }
--- LinkedListUtil.java
+++ LinkedListUtil.java
@@ -2,22 +2,54 @@ public class LinkedListUtil {
     
     //returns the length of the linked list
     public static int length(LinkedList value){
-        return -1;
+        int count = 0;
+        Node nuts = value.getFront();
+        while(nuts.getNext()!=null){
+            nuts=nuts.getNext();
+            count++;
+        }
+        return count;
     }
 
     //returns the index of the first node with given data value, -1 otherwise
     public static int search(LinkedList value, int data){
+        int index =0;
+        Node nuts = value.getFront();
+        while(nuts.getNext()!=null){
+            if(nuts.getData()==data){
+                return index;
+            }
+            nuts=nuts.getNext();
+            index++;
+        }
         return -1;
     }
 
     //returns the node at a given index, null if index out of bounds
     public static Node getNode(LinkedList value, int index){
+        Node nuts = value.getFront();
+        int i =0;
+        while(i<index&&nuts.getNext()!=null){
+            nuts=nuts.getNext();
+            i++;
+            if(index-i ==1){
+                return nuts;
+            }
+        }
         return null;
     }
 
     //returns the number of times data occurs in the linked list
     public static int count(LinkedList value, int data){
-        return -1;
+        int count =0;
+        Node nuts = value.getFront();
+        while(nuts.getNext()!=null){
+            if(nuts.getData()==data){
+                count++;
+            }
+            nuts=nuts.getNext();
+        }
+        return count;
     }
 
     //determines if two lists contain the same data values
